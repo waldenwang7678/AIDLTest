238b83b3de1d149f14fd9a15b705df885c19f7f0
wangjitao <767884538@qq.com>
2017-08-09 18:09:02 +0800

    添加 messenger

--- app/src/main/java/com/wangjt/aidltest/MainActivity.java
+++ app/src/main/java/com/wangjt/aidltest/MainActivity.java
@@ -5,10 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.walden.aidl.MyAidl;
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MyAidl aidl;
     boolean useAIDL = false;   //进程通信, 使用或不使用 aidl ,
     IBinder binder;
+    Messenger mMessenger;
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
@@ -37,6 +43,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(MainActivity.this, "连接失败", Toast.LENGTH_SHORT).show();
         }
     };
+    private ServiceConnection conMessenger = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mMessenger = new Messenger(iBinder);
+            //todo
+            
+            Toast.makeText(MainActivity.this, "messenger连接成功", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mMessenger = null;
+            Toast.makeText(MainActivity.this, "messenger连接失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Bundle bundle = msg.getData();
+            if (bundle != null) {
+                String str = bundle.getString("address");
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,23 +82,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.unbindService).setOnClickListener(this);
         findViewById(R.id.add).setOnClickListener(this);
         findViewById(R.id.min).setOnClickListener(this);
+        findViewById(R.id.messenger_connect).setOnClickListener(this);
+        Switch swButton = (Switch) findViewById(R.id.switch1);
+        swButton.setChecked(true);
+        swButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    useAIDL = true;
+                } else {
+                    useAIDL = false;
+                }
+                connect();  //切换一次就连接一次
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bindService:
-                Intent intent = new Intent();
-                if (useAIDL) {
-                    intent.setAction("com.walden.aidl");   //服务端提供的服务
-                } else {
-                    intent.setAction("com.walden.aidl_no_aidl");  //不使用 AIDL
-                }
-                intent.setPackage("com.wangjt.aidlservice");
-                bindService(intent, connection, Context.BIND_AUTO_CREATE);  //连接到一个服务
+                connect();  //连接
                 break;
             case R.id.unbindService:
                 unbindService(connection);  //断开连接
+                Toast.makeText(this, "断开连接", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.add:
 
@@ -135,6 +175,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.makeText(this, "服务端未绑定或被异常杀死，请重新绑定服务端", Toast.LENGTH_SHORT).show();
                 }
                 break;
+            case R.id.messenger_connect:
+                Intent intent = new Intent();
+                intent.setAction("com.walden.messenger");  //连接 messenger service
+                intent.setPackage("com.wangjt.aidlservice");
+                bindService(intent, conMessenger, Context.BIND_AUTO_CREATE);  //连接到一个服务
+                break;
         }
+    }
+
+    private void connect() {
+        Intent intent = new Intent();
+        if (useAIDL) {
+            intent.setAction("com.walden.aidl");   //服务端提供的服务
+        } else {
+            intent.setAction("com.walden.aidl_no_aidl");  //不使用 AIDL
+        }
+        intent.setPackage("com.wangjt.aidlservice");
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);  //连接到一个服务
     }
 }
